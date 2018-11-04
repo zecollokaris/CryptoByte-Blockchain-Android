5b63774b95139f4689c82c849939f45545b18e45
zecollokaris <collo.kariss@gmail.com>
2018-11-04 21:03:44 +0300

    creating arrays

---
+++ CryptoCurrency/app/src/main/java/com/zecollokaris/cryptocurrency/SliderAdapter.java
@@ -0,0 +1,29 @@
+package com.zecollokaris.cryptocurrency;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    //Arrays
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return false;
+    }
+}
