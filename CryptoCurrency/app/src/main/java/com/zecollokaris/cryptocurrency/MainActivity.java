2b532f8c4ca232d006aae6171f89d876e1e4c879
zecollokaris <collo.kariss@gmail.com>
2018-11-04 20:28:48 +0300

    new layout created

--- CryptoCurrency/app/src/main/java/com/zecollokaris/cryptocurrency/MainActivity.java
+++ CryptoCurrency/app/src/main/java/com/zecollokaris/cryptocurrency/MainActivity.java
@@ -1,13 +1,23 @@
 package com.zecollokaris.cryptocurrency;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //VIEWSLIDERS
+        mSlideViewPager = (ViewPager) findViewById(R.id.SlideViewPager);
+        mDotsLayout = (LinearLayout) findViewById(R.id.DotsLayout);
+
     }
 }
