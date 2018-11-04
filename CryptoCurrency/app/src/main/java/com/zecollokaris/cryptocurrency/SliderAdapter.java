bd404e3b0330bddacb0dc7391f9ae3f575856de4
zecollokaris <collo.kariss@gmail.com>
2018-11-04 21:30:27 +0300

    initalizing view for images to be vissible

--- CryptoCurrency/app/src/main/java/com/zecollokaris/cryptocurrency/SliderAdapter.java
+++ CryptoCurrency/app/src/main/java/com/zecollokaris/cryptocurrency/SliderAdapter.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class SliderAdapter extends PagerAdapter {
 
@@ -15,7 +18,8 @@ public class SliderAdapter extends PagerAdapter {
         this.context = context;
     }
 
-    //Arrays
+//DETAILS TO BE SHOWN ON THE SLIDERS!!
+    //IMAGE SLIDER
     public int [] slideimages = {
 
             R.drawable.cryptologo1,
@@ -23,21 +27,43 @@ public class SliderAdapter extends PagerAdapter {
             R.drawable.cryptologo4
     };
 
+    //HEADING SLIDER
     public String [] slideheading = {
 
             "EAT",
             "SLEEP",
             "CODE"
+    };
 
+    //TEXT SLIDER
+    public String [] slidedesc ={
+
+            "First lorem to test out Code Lorem ipsumaslhflasfn lasfnlaskdbf lsa flasnfs fkasnadk asdkandasdna sdlaskd;adma; dalsdn klsadasd kakskda;sd lasdlaskd admlas dasd" +
+                    "first",
+            "Second lorem to test out Code Lorem ipsumaslhflasfn lasfnlaskdbf lsa flasnfs fkasnadk asdkandasdna sdlaskd;adma; dalsdn klsadasd kakskda;sd lasdlaskd admlas dasd" +
+                    "second",
+            "Third lorem to test out Code Lorem ipsumaslhflasfn lasfnlaskdbf lsa flasnfs fkasnadk asdkandasdna sdlaskd;adma; dalsdn klsadasd kakskda;sd lasdlaskd admlas dasd" +
+                    "third",
     };
 
     @Override
     public int getCount() {
-        return 0;
+        return slideheading.length;
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-        return false;
+        return view == (RelativeLayout) o;
     }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById()
+    }
+
 }
