36d6b860e3bfd02b3f590dcb91032fde9f60b8f6
zecollokaris <collo.kariss@gmail.com>
2018-11-05 13:09:08 +0300

    added splash page

--- app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.zecollokaris.cryptocurrency;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +10,20 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    Thread myThread = new Thread(){
+        @Override
+        public void run(){
+            try {
+                sleep(3000);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+        myThread.start();
 
-        Tread myThread = new Thread()
     }
 }
