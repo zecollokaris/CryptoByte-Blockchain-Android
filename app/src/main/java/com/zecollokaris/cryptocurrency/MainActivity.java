0de7ac7cc2ac3e56239fa94056f23a34a283a1dc
zecollokaris <collo.kariss@gmail.com>
2018-11-06 01:10:09 +0300

    done with design for now

--- app/src/main/java/com/zecollokaris/cryptocurrency/MainActivity.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
             mDots[i] = new TextView(this);
             mDots[i].setText(Html.fromHtml("&#8226;"));
             mDots[i].setTextSize(35);
-            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentPink));
 
             mDotLayout.addView(mDots[i]);
         }
