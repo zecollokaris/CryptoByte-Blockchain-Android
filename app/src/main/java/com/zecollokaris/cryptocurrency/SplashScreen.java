c785619182d429715e667f7d4e30f58550ef2d76
zecollokaris <collo.kariss@gmail.com>
2018-11-05 12:45:44 +0300

    added image and title on splash screen

--- app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
@@ -9,5 +9,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Tread myThread = new Thread()
     }
 }
