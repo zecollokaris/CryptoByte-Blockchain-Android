42aba3952c4af25a28736e4f91c6fbd166073942
zecollokaris <collo.kariss@gmail.com>
2018-11-11 19:48:10 +0300

    adding new pager adapter

--- app/src/main/java/com/zecollokaris/cryptocurrency/LoginAdapter.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/LoginAdapter.java
@@ -1,4 +1,26 @@
 package com.zecollokaris.cryptocurrency;
 
-public class LoginAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+
+public class LoginAdapter extends FragmentStatePagerAdapter {
+
+    int mNoOfTabs;
+
+    public LoginAdapter(FragmentManager fm, int NumberOfTabs){
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
