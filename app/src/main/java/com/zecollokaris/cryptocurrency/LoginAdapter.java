b08280c81720bff232dac062d1b59d8319b69062
zecollokaris <collo.kariss@gmail.com>
2018-11-11 20:13:39 +0300

    defined slider activity from tab 1 to tab 2

--- app/src/main/java/com/zecollokaris/cryptocurrency/LoginAdapter.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/LoginAdapter.java
@@ -15,12 +15,25 @@ public class LoginAdapter extends FragmentStatePagerAdapter {
     }
 
     @Override
-    public Fragment getItem(int i) {
-        return null;
+    public Fragment getItem(int position) {
+        switch (position) {
+            //Slide to Tab1
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            //Slide to tab2
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+
+            default:
+                return null;
+
+        }
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mNoOfTabs;
     }
 }
