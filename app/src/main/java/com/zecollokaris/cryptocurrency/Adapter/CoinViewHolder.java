fa2090f2f0c33b9248fd9f087e4c6bcc58e284dd
zecollokaris <collo.kariss@gmail.com>
2018-11-13 11:46:43 +0300

    created class CoinViewHolder

---
+++ app/src/main/java/com/zecollokaris/cryptocurrency/Adapter/CoinViewHolder.java
@@ -0,0 +1,12 @@
+package com.zecollokaris.cryptocurrency.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class CoinViewHolder extends RecyclerView.ViewHolder {
+    
+
+    public CoinViewHolder (View itemView) {
+        super(itemView);
+    }
+}
