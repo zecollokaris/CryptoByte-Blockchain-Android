2d34a5f2b60289aa397f2ebe0607943b1345022c
zecollokaris <collo.kariss@gmail.com>
2018-11-13 12:36:27 +0300

    implemented class coin adapter

--- app/src/main/java/com/zecollokaris/cryptocurrency/Adapter/CoinAdapter.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/Adapter/CoinAdapter.java
@@ -1,6 +1,23 @@
 package com.zecollokaris.cryptocurrency.Adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class CoinAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
