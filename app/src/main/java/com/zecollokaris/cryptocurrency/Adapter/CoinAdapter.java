73096445dd8d710cf5b58a7465c196d76d6aa0b6
zecollokaris <collo.kariss@gmail.com>
2018-11-13 16:32:26 +0300

    parseing in display colors

--- app/src/main/java/com/zecollokaris/cryptocurrency/Adapter/CoinAdapter.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/Adapter/CoinAdapter.java
@@ -1,6 +1,7 @@
 package com.zecollokaris.cryptocurrency.Adapter;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.picasso.Picasso;
 import com.zecollokaris.cryptocurrency.Interface.ILoadMore;
 import com.zecollokaris.cryptocurrency.Model.CoinModel;
 import com.zecollokaris.cryptocurrency.R;
@@ -71,6 +73,13 @@ public class CoinAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         holderItem.seven_days_change.setText(item.getPercentage_change_7d()+"%");
 
         //Load Images (Picasso)
+        Picasso.with(activity)
+                .load(new StringBuilder("https://res.cloudinary.com/dxi90ksom/image/upload/")
+                .append(item.getSymbol().toLowerCase()).append(".png").toString())
+                .into(holderItem.coin_icon);
+
+        holderItem.one_hour_change.setTextColor(item.getPercentage_change_1h().contains("-")?
+                Color.parseColor("#FF0000"):Color.parseColor("32CD32"));
 
     }
 
