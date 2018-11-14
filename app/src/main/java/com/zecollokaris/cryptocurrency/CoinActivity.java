695f6876036f2db52a48f8d0922f0bb2b58955e3
zecollokaris <collo.kariss@gmail.com>
2018-11-14 11:18:54 +0300

    working on slide to refresh

--- app/src/main/java/com/zecollokaris/cryptocurrency/CoinActivity.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/CoinActivity.java
@@ -5,16 +5,23 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.zecollokaris.cryptocurrency.Adapter.CoinAdapter;
 import com.zecollokaris.cryptocurrency.Interface.ILoadMore;
 import com.zecollokaris.cryptocurrency.Model.CoinModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class CoinActivity extends AppCompatActivity {
 
@@ -63,9 +70,41 @@ public class CoinActivity extends AppCompatActivity {
         adapter.setiLoadMore(new ILoadMore() {
             @Override
             public void onLoadMore() {
-                if (items,size() <= 1000)
+                if (items.size() <= 1000)//Max Size is 1000 coins
+                {
+                   loadNext10coin(items.size());
+                }
+                else
+                {
+                    Toast.makeText(CoinActivity.this, "Max items is 1000", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private void loadNext10coin(int index) {
+        client = new OkHttpClient();
+        request = new Request.Builder().url(String.format("https://api.coinmarketcap.com/v1/ticker/?start=%d&limit=10",index))
+                .build();
+
+        swipeRefreshLayout.setRefreshing(true);
+        client.newCall(request)
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Toast.makeText(CoinActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+                        String body = response.body().toString();
+                        Gson gson = new Gson();
+                        List<CoinModel> newitems = gson.fromJson(body,new TypeToken<List<CoinModel>>(){}.getType());
+
+                        runOnUiThread();
+                    }
+                });
     }
 
     private void loadFirst10Coin(int i) {
