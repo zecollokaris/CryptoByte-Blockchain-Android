e149249ac4791a85580aa7de0d125e8a4d5a4a0a
zecollokaris <collo.kariss@gmail.com>
2018-11-14 10:57:35 +0300

    set max size to 1000

--- app/src/main/java/com/zecollokaris/cryptocurrency/CoinActivity.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/CoinActivity.java
@@ -3,9 +3,11 @@ package com.zecollokaris.cryptocurrency;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.zecollokaris.cryptocurrency.Adapter.CoinAdapter;
+import com.zecollokaris.cryptocurrency.Interface.ILoadMore;
 import com.zecollokaris.cryptocurrency.Model.CoinModel;
 
 import java.util.ArrayList;
@@ -32,7 +34,42 @@ public class CoinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coin_main);
 
-        swipeRefreshLayout = (SwipeRefreshLayout)
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.rootLayout);
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                loadFirst10Coin(0);
+            }
+        });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                items.clear();
+                loadFirst10Coin(0);
+                setupAdapter();
+            }
+        });
+
+        recyclerView = (RecyclerView)findViewById(R.id.coinList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        setupAdapter();
 
     }
+
+    private void setupAdapter() {
+        adapter = new CoinAdapter(recyclerView,CoinActivity.this,items);
+        recyclerView.setAdapter(adapter);
+        adapter.setiLoadMore(new ILoadMore() {
+            @Override
+            public void onLoadMore() {
+                if (items,size() <= 1000)
+            }
+        });
+    }
+
+    private void loadFirst10Coin(int i) {
+    }
+
+
 }
