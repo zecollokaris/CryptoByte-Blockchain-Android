eb22d0cb7eaccbf948d7e70adc138eedfca0ac01
zecollokaris <collo.kariss@gmail.com>
2018-11-21 14:21:03 +0300

    initialized button n edit texts to be used

--- app/src/main/java/com/zecollokaris/cryptocurrency/Tab1.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/Tab1.java
@@ -8,16 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Tab1 extends Fragment {
+    EditText email, password;
+    //Login Button!
     Button mLoginBtn;
 
-    Firebase auth
+    FirebaseAuth auth;
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tab1, container, false);
+
+        auth = FirebaseAuth.getInstance();
+
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+        mLoginBtn=(Button) view.findViewById(R.id.loginbtn);
+
     }
 }
