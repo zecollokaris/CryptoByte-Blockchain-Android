82c62d081a555113e150441c2a37a0fbb6945e89
zecollokaris <collo.kariss@gmail.com>
2018-11-21 14:07:42 +0300

    done with register

--- app/src/main/java/com/zecollokaris/cryptocurrency/Tab1.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/Tab1.java
@@ -7,9 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Tab1 extends Fragment {
+    Button mLoginBtn;
+
+    Firebase auth
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tab1, container, false);
