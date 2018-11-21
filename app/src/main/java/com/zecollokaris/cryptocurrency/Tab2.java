9ecafbee30f4a56b1ded337bc548c3b73deeae32
zecollokaris <collo.kariss@gmail.com>
2018-11-21 13:24:43 +0300

    added register toasts for error handling

--- app/src/main/java/com/zecollokaris/cryptocurrency/Tab2.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/Tab2.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -40,23 +42,51 @@ public class Tab2 extends Fragment {
           mRegisterBtn.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View v) {
+                  String txt_username = username.getText().toString();
+                  String txt_email = email.getText().toString();
+                  String txt_password = password.getText().toString();
 
+                  if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                      Toast.makeText(getContext(), "All Fields are required!",Toast.LENGTH_SHORT).show();
+                  } else if (txt_password.length() < 6) {
+                      Toast.makeText(getContext(), "Password must be atleast 6 characters!",Toast.LENGTH_SHORT).show();
+                  } else {
+                      register(txt_username, txt_email, txt_password);
+                  }
               }
           });
           return view;
     }
 
-    private void register(String username, String email,String password){
+    private void register(final String username, String email, String password){
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(getActivity(),new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     FirebaseUser firebaseUser = auth.getCurrentUser();
+                    assert firebaseUser != null;
                     String userid = firebaseUser.getUid();
 
                     reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
-                    HashMap<String, String> hashMap = new HashMap<>()
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("username", username);
+                    hashMap.put("imageURL", "default");
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(getContext(), CoinActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                getActivity().finish();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getContext(), "You can't register with this email or password",Toast.LENGTH_SHORT).show();
                 }
             }
         });
