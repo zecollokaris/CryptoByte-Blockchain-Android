982ab44c1c15b6ef5635d98cbcc8edce34e4a0ca
zecollokaris <collo.kariss@gmail.com>
2018-11-25 00:42:18 +0300

    added font but commented it out as it did not work but will fix it

--- app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
@@ -1,15 +1,27 @@
 package com.zecollokaris.cryptocurrency;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SplashScreen extends AppCompatActivity {
+
+
+//    TextView text;
+//    Typeface tfc1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+//        text = (TextView) findViewById(R.id.splashText);
+//        tfc1 = Typeface.createFromAsset(getAssets(), "fonts/limelight.ttf");
+//        text.setTypeface(tfc1);
+
     Thread myThread = new Thread(){
         @Override
         public void run(){
@@ -27,3 +39,16 @@ public class SplashScreen extends AppCompatActivity {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
