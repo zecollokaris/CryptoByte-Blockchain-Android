4bb2e56bd9fe1ef50c685af70ce59cea2e51c47b
zecollokaris <collo.kariss@gmail.com>
2018-11-26 17:21:43 +0300

    changed items id in nav bar

--- app/src/main/java/com/zecollokaris/cryptocurrency/CoinActivity.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/CoinActivity.java
@@ -1,8 +1,10 @@
 package com.zecollokaris.cryptocurrency;
 
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -10,12 +12,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
@@ -37,7 +36,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class CoinActivity extends AppCompatActivity {
+public class CoinActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     @BindView(R.id.coinToolbar) Toolbar toolbar;
     List<CoinModel> items = new ArrayList<>();
@@ -76,12 +75,24 @@ public class CoinActivity extends AppCompatActivity {
         setupAdapter();
 
         drawer =  findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_prof:
+
+                break;
+        }
+        return true;
     }
 
     @Override
