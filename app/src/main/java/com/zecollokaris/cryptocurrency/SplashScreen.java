3ce69f44d4d98e86055bf5a2a049b118e74ad6af
zecollokaris <collo.kariss@gmail.com>
2018-11-27 09:45:00 +0300

    added text comments for animations inside Splashscreen.java for easy later use incase it is needed

--- app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
+++ app/src/main/java/com/zecollokaris/cryptocurrency/SplashScreen.java
@@ -23,15 +23,27 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         ButterKnife.bind(this);
 
+//      THE CONTENTS ARE PUT INSIDE A LINEAR LAYOUT.
+//      We reference the image and text using their id.
+//      descimage = (R.id.titleimage)
+//      desctxt = (R.id.titletxt)
         descimage = (LinearLayout) findViewById(R.id.titleimage);
         desctxt = (LinearLayout) findViewById(R.id.titletxt);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
         downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+//      THIS ANIMATIONS ARE SET INSIDE THE (ANIM) FOLDER.
+//      This initiaizes the animations.
+
+//      //\\ NOTE! //\\
+//      FOR THIS ANIMATION TO WORK CONTENTS MUST BE INSIDE LINEAR LAYOUT
+//      CHECK...-> activity_splash_screen.xml for reference!
         descimage.setAnimation(downtoup);
         desctxt.setAnimation(uptodown);
 
 
-
+//  THIS CODE ROTATES IMAGE! AS AN ANIMATION!
+//        mLogo - Is used after we bind with the ID.
+//        Id = imageView2
     RotateAnimation rotate = new RotateAnimation(0, 720, Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
     rotate.setDuration(3000);
     rotate.setInterpolator(new LinearInterpolator());
